965cc1a4c058bd1f7760cd45a0474b098cc55206
Sohaibislam45 <sohaibislam45@gmail.com>
2025-12-07 20:23:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+//uri and client here
+//sohaib
+//test123
+
+const uri =
+  "mongodb+srv://sohaib:test123@cluster0.4lapvpm.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("vehicles-db");
+    const vehicles = db.collection("vehicles");
+
+    app.get("/vehicles", async (req, res) => {
+      try {
+        const data = await vehicles.find().toArray();
+        res.send(data);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
